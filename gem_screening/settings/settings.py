5a8576f067a81c9b9d92481d599379a27a8cb885
BennyGinger <benoit.roux@gmail.com>
2025-10-23 18:02:35 +0200

    set up setting for boldi

--- gem_screening/settings/settings.py
+++ gem_screening/settings/settings.py
@@ -7,31 +7,32 @@ savedir = r'D:\Boldi\Tg-c1864v2-lib72-GFPpos'
 savedir_name = '60k-for-CellSorter'
 # Aquisition settings for the microscope
 aqui_sets = AcquisitionSettings(
-                    objective='20x',
-                    lamp_name='pE-4000',)
+                    objective='20x',)
 
 # Settings for the dish used in the imaging process
 dish_sets = DishSettings(
                     dish_name='35mm',
-                    # well_selection=['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12'],
-                    well_selection=['A1'],
-                    af_method='Manual',
-                    overwrite_autofocus=True,
+                    well_selection=['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',],
+                    # well_selection=['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',],
+                    # well_selection=['A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3',],
+                    # well_selection=['A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4',],
+                    # well_selection=['A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5',],
+                    # well_selection=['A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6',],
+                    # well_selection=['A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7',],
+                    # well_selection=['A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8',],
+                    # well_selection=['A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9',],
+                    # well_selection=['A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10',],
+                    # well_selection=['A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11',],
+                    # well_selection=['A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12',],
+                    af_method='sq_grad',
+                    overwrite_autofocus=False,
                     overwrite_calib=False,
-                    # numb_field_view=1,
-                    numb_field_view=5,
+                    numb_field_view=None,
                     dmd_window_only=False,
                     )
 
 # Preset settings for imaging for measurement
 measure_sets = MeasureSettings(
-                    # preset_measure=PresetMeasure(
-                    #                 optical_configuration='iRed',
-                    #                 intensity=5),
-                    # do_refseg=True,
-                    # preset_refseg=PresetRefseg(
-                    #                 optical_configuration='GFP',
-                    #                 intensity=25))
                     preset_measure=PresetMeasure(
                                     optical_configuration='GFP',
                                     intensity=25),
